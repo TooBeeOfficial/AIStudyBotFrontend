bb3438834248d63ea2bf0666ae943e6222cddb56
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2026-06-22 12:19:23 +0300

    implemented fisher-yates shuffle if there is a max number of questions. Added a finished check to display the quiz end.

--- src/app/pages/take-quiz/take-quiz.ts
+++ src/app/pages/take-quiz/take-quiz.ts
@@ -13,23 +13,50 @@ import { MessageDialogComponent } from '../../shared/dialogs/success-dialog/succ
 })
 export class TakeQuiz implements OnInit {
   quizService: QuizService = inject(QuizService);
-  myQuiz: QuestionModel[] = [];
-  currentQuestion: QuestionModel = new QuestionModel();
-  questionsFinished: number = 0;
-  showResults: boolean = false;
-  showResultsOnEnd: boolean = false;
-  selectedAnswer: number = -1;
   dialog: MatDialog = inject(MatDialog);
+
+  currentQuestion: QuestionModel = new QuestionModel();
+  myQuiz: QuestionModel[] = [];
+
+  showResultsOnEnd: boolean = false;
+  showResults: boolean = false;
+  finish: boolean = false;
+
   totalCorrectAnswer: number = 0;
+  questionsFinished: number = 0;
+  selectedAnswer: number = -1;
+  maxQuestionsForQuiz: number = 3;
 
   ngOnInit(): void {
     this.quizService.quiz$.subscribe({
       next: (quiz) => {
         if (!quiz) return;
         this.currentQuestion = quiz.questions[0];
-        this.myQuiz = quiz.questions;
+        if (this.maxQuestionsForQuiz === -1 && this.maxQuestionsForQuiz >= quiz.questions.length) {
+          this.myQuiz = quiz.questions;
+        } else {
+          this.myQuiz = this.getRandomItems(quiz.questions, this.maxQuestionsForQuiz);
+        }
       },
     });
+  }
+
+  // fisher-yates shuffle
+  // gets requested amount of questions from given array
+  // shuffles list then returns first 10 questions
+  getRandomItems<QuestionModel>(
+    questions: QuestionModel[],
+    numberOfQuestions: number,
+  ): QuestionModel[] {
+    const copy = [...questions];
+
+    // swap
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+
+    return copy.slice(0, numberOfQuestions);
   }
 
   checkCorrectAnswer(answer: number) {
@@ -72,6 +99,8 @@ export class TakeQuiz implements OnInit {
 
     if (this.questionsFinished + 1 < this.myQuiz.length) {
       this.questionsFinished += 1;
+    } else {
+      this.finish = true;
     }
     this.currentQuestion = this.myQuiz[this.questionsFinished];
   }
